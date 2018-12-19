ac767c3d0587487de9ba3f3014a1a5365b044aab
kartheekmunaga <45980480+kartheekmunaga@users.noreply.github.com>
2018-12-19 14:44:02 -0500

    Create backup-nsg.ps1

---
+++ backup-nsg.ps1
@@ -0,0 +1,24 @@
+Import-Module AzureRM
+
+#To select particular sub
+$subcriptions = get-content 'C:\Users\kmunaga001\Documents\Projects\Astro\NSG Backups\subcriptions.txt'
+
+Foreach($subcription in $subcriptions)
+{
+   select-azurermsubscription –SubscriptionId $subcription
+
+   $nsgs = Get-AzureRmNetworkSecurityGroup
+   $exportPath = 'NSG Backups'
+
+   #backup nsgs to csv
+       Foreach ($nsg in $nsgs) 
+       {
+         New-Item -ItemType file -Path "$exportPath\$($nsg.Name).csv" -Force
+         $nsgRules = $nsg.SecurityRules
+         foreach ($nsgRule in $nsgRules) 
+         {
+         $nsgRule | Select-Object Name,Description,Priority,@{Name=’SourceAddressPrefix’;Expression={[string]::join(“,”, ($_.SourceAddressPrefix))}},@{Name=’SourcePortRange’;Expression={[string]::join(“,”, ($_.SourcePortRange))}},@{Name=’DestinationAddressPrefix’;Expression={[string]::join(“,”, ($_.DestinationAddressPrefix))}},@{Name=’DestinationPortRange’;Expression={[string]::join(“,”, ($_.DestinationPortRange))}},Protocol,Access,Direction `
+         | Export-Csv "$exportPath\$($nsg.Name).csv" -NoTypeInformation -Encoding ASCII -Append
+         }
+       }
+}
